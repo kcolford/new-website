2df5ec2b697814162cb28a3cca1955c5db9f73ac
Kieran Colford <kieran@kcolford.com>
2020-05-12 19:48:03 -0400

    added stuff

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+[![Netlify Status](https://api.netlify.com/api/v1/badges/3448007f-adc9-4424-b665-4c921a90e41c/deploy-status)](https://app.netlify.com/sites/hardcore-knuth-a8f8e9/deploys)
+
 # Personal Website
 
 Just a basic personal website.
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,6 @@
 let remarkPlugins = [
   `gatsby-remark-copy-linked-files`,
+  `gatsby-remark-smartypants`,
   {
     resolve: `gatsby-remark-images`,
     options: { maxWidth: 800 },
@@ -41,14 +42,28 @@ module.exports = {
       resolve: `gatsby-plugin-google-tagmanager`,
       options: { id: "GTM-KN3M4BF" },
     },
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: { trackingId: "UA-45606820-1" },
-    },
     `gatsby-plugin-catch-links`,
-    `gatsby-plugin-robots-txt`,
     `gatsby-plugin-sitemap`,
-    `gatsby-plugin-canonical-urls`,
+    {
+      resolve: `gatsby-plugin-react-helmet-canonical-urls`,
+      options: {
+        // siteUrl: `https://www.kcolford.com`,
+      },
+    },
+    `gatsby-plugin-robots-txt`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Kieran Colford`,
+        short_name: `kcolford`,
+        start_url: `/`,
+        background_color: `#000000`,
+        theme_color: `#ffffff`,
+        display: `standalone`,
+        icon: `src/images/gatsby-icon.png`,
+      },
+    },
+    `gatsby-plugin-offline`,
     `gatsby-plugin-netlify`,
   ],
 }
