1976482312241626dd2be2c40ef4359a86c76199
Kieran Colford <kieran@kcolford.com>
2020-05-12 16:45:50 -0400

    added a bunch of important stuff

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,28 +37,14 @@ module.exports = {
 	plugins: remarkPlugins,
       },
     },
-    `gatsby-plugin-catch-links`,
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: { trackingId: "UA-45606820-1" },
     },
+    `gatsby-plugin-catch-links`,
     `gatsby-plugin-robots-txt`,
     `gatsby-plugin-sitemap`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-	name: `gatsby-starter-default`,
-	short_name: `starter`,
-	start_url: `/`,
-	background_color: `#663399`,
-	theme_color: `#663399`,
-	display: `minimal-ui`,
-	icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-canonical-urls`,
     `gatsby-plugin-netlify`,
   ],
 }
