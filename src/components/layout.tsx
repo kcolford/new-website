c0d7f4d2cf4f4d15240af67e2b1c7bb5796491d4
Kieran Colford <kieran@kcolford.com>
2020-05-13 17:07:19 -0400

    adds Title element to layout

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -2,12 +2,20 @@ import * as React from "react"
 import * as PropTypes from "prop-types"
 import SEO from "./seo"
 import { Container } from "@material-ui/core"
+import { useStaticQuery, graphql } from "gatsby"
 
 const Layout = ({ children, _frontmatter }) => {
   let { title, description } = _frontmatter
+  const data = useStaticQuery(graphql`{
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }`)
   return (
     <Container maxWidth="md">
-      <SEO title={title} description={description} />
+      <SEO title={title || data.site.siteMetadata.title} description={description} />
       <main>{children}</main>
     </Container>
   )
